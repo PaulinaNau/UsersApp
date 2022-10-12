f721825bd326829fe0d9b4b7680fbb8002308d9e
PaulinaNau <paulina.naurecka@gmail.com>
2022-10-12 15:27:59 +0200

    basic setup - prettier, eslint, absolute import, husky, lint-stage

---
+++ src/views/Root.js
@@ -0,0 +1,13 @@
+import './Root.css';
+
+function Root() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Start building your App</p>
+      </header>
+    </div>
+  );
+}
+
+export default Root;
